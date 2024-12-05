1a19de72b8b0de5ecfe268fa1f140d7452ea7122
chanlika servais <chanlika.servais@gmail.com>
2024-12-05 12:55:45 +0100

    afficher les étoiles de rating de chaque ninja

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_12_04_133105) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_05_114701) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,9 +52,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_12_04_133105) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
+    t.integer "price"
     t.float "latitude"
     t.float "longitude"
-    t.integer "price"
+    t.integer "review"
+    t.integer "rating"
     t.index ["user_id"], name: "index_ninjas_on_user_id"
   end
 
