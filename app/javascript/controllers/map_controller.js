6244e19d83d3c24f949b9badaaa0340c8a9d3f69
marie beliben <mbeliben@icloud.com>
2024-12-05 16:58:04 +0100

    map-changer

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from 'mapbox-gl' // Don't forget this!
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 // Connects to data-controller="map"
 
 
@@ -12,13 +13,15 @@ export default class extends Controller {
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
 
-
-    this.map = new mapboxgl.Map({
+      this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
       this.#addMarkersToMap()
       this.#fitMapToMarkers()
+
+      this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl }))
 
   }
 
@@ -41,7 +44,7 @@ export default class extends Controller {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([ marker.lng, marker.lat ]))
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
-    
+
   }
 
 }
